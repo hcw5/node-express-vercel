4bb781bae2f07f07416f94c3585c40b5ba13401d
azeddine chakir <77072940+hcw5@users.noreply.github.com>
2023-11-16 14:38:13 +0100

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -2,15 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
+  // First handler for "/"
   return res.status(200).json({
     title: "Express Testing",
     message: "The app is working properly!",
   });
 });
 
-router.get("/", async (req, res, next) => {
+router.get("/head", async (req, res, next) => {
+  // Second handler for "/head"
   const headers = req.headers;
   return res.status(200).json(headers);
 });
+
 
 module.exports = router;
