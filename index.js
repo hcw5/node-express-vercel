ef51d208c028462c28ba113a53c19364856266f5
azeddine chakir <77072940+hcw5@users.noreply.github.com>
2023-11-16 01:16:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.use(express.json());
 // Routes
 app.use("/home", home);
 
+app.get("/", (req, res) => (
+  res.send("Omg finally")
+  )):
+
 // connection
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
